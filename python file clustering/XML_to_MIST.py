713ceb0c8a8067407b33740159de5851ef6c0ada
R3dtechsGame <maya.schlesinger@gmail.com>
2016-07-10 16:30:18 +0300

    added the crazy german doctor's cuckoo to MIST translate code

---
+++ python file clustering/XML_to_MIST.py
@@ -0,0 +1,2 @@
+# This code is used to convert xml behavioural representation as reported by CWSandbox to MIST format.
+# MIST instruction are composed of different fields: CATEGORY, OPERATION, and several ARGBLOCKS.
