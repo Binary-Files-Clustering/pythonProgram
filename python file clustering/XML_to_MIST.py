26e156a67308de7d6bc1a8317f194ebf89839940
R3dtechsGame <maya.schlesinger@gmail.com>
2016-07-11 11:04:43 +0300

    Finally figured out the MIST conversion, added the python conversion files

--- python file clustering/XML_to_MIST.py
+++
@@ -1,2 +0,0 @@
-# This code is used to convert xml behavioural representation as reported by CWSandbox to MIST format.
-# MIST instruction are composed of different fields: CATEGORY, OPERATION, and several ARGBLOCKS.
