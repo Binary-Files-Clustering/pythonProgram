335a6ffb53ca3531a266d0de1bd4fcf0e0bdfd8e
R3dtechsGame <maya.schlesinger@gmail.com>
2016-09-18 20:42:49 +0300

    added some python code(including SparseVector class, a vector space module and a clustering algorithm module.

---
+++ algorithms/mist_vectors.py
@@ -0,0 +1,11 @@
+class SparseVector:
+    def __init__(self):
+        self.values = {}
+
+    def __getitem__(self, item):
+        if item in self.values:
+            return self.values[item]
+        return 0
+
+    def __setitem__(self, key, value):
+        self.values[key] = value
